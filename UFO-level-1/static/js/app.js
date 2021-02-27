4ca40ec926ea870ea6cc0f0c8331d68dac83f570
Ashley Dixon <ashleydixon2012@gmail.com>
2021-02-27 15:17:38 -0700

    Console log the key and corresponding value

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -9,5 +9,8 @@ console.log(tableData);
 // Iterate through data and insert into table
 data.forEach(function(ufosighting) {
     console.log(ufosighting);
-
+    var rows = tbody.append("tr");
+    Object.entries(ufosighting).forEach(function([key, value]) {
+        console.log(key,value);
+    })
 })
