9b07a119546b6bd459baf78ca5082183a05ada31
Ashley Dixon <ashleydixon2012@gmail.com>
2021-02-27 15:45:17 -0700

    Table filters as soon as matching data is entered

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -26,7 +26,23 @@ function filtertable() {
     // Variables
     var input, filter, table, tr, td, i, txtValue;
     input = document.getElementById("myInput");
-    filter = input.value();
+    filter = input.value;
     table = document.getElementById("ufo-table");
     tr = table.getElementsByTagName("tr");
+
+    // For loop to iterate through rows
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+
+        // Conditional to hide data that does not match filter
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            }
+            else {
+                tr[i].style.display = "none"
+            }
+        }
+    }
 }
